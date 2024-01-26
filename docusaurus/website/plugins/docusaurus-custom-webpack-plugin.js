e5a757050dec60c51a4e48bc8d3fed4f1c5c3ddb
xxy <1060195480@qq.com>
2024-01-26 14:54:40 +0800

    123

--- docusaurus/website/plugins/docusaurus-custom-webpack-plugin.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (_, options) {
-  const { id, ...opts } = options || {};
-  return {
-    name: 'docusaurus-custom-webpack-plugin',
-    configureWebpack(config, isServer, utils) {
-      return opts;
-    },
-  };
-};
--- packages/create-plugin/src/commands/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './generate.command';
-export * from './update.command';
-export * from './migrate.command';
-export * from './version.command';
